3ceae1216566d6bb50a2a082b0c5915415376dbd
Gofaraway71 <88510377+Gofaraway71@users.noreply.github.com>
2021-08-25 11:00:32 +0100

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,13 @@
 #
-# Copyright (C) 2017 The Android Open Source Project
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,113 +16,185 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/xiaomi/markw
+DEVICE_PATH := device/xiaomi/merlin
 
-## Architecture
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a75
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+TARGET_USES_64_BIT_BINDER := true
 
-TARGET_BOARD_PLATFORM := msm8953
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
-TARGET_BOARD_SUFFIX := _64
+# Assert
+TARGET_OTA_ASSERT_DEVICE := RMX2151
 
-## Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := markw
-TARGET_NO_BOOTLOADER := true
+# AVB
+BOARD_AVB_ENABLE := true
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
-## Crypto
-TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
-TARGET_HW_DISK_ENCRYPTION := true
+#MIUI 12.xx A11 DECRYPTION
+BOARD_AVB_RECOVERY_ADD_HASH_FOOTER_ARGS += \
+    --prop com.android.build.boot.os_version:$(PLATFORM_VERSION) \
+    --prop com.android.build.boot.security_patch:$(PLATFORM_SECURITY_PATCH)
+
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_COPY_OUT_VENDOR := vendor
+RECOVERY_SDCARD_ON_DATA := true
+TW_INCLUDE_NTFS_3G := true
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SUPER_PARTITION_SIZE := 6012043264
+BOARD_SUPER_PARTITION_GROUPS := main
+BOARD_MAIN_SIZE := 6012043264
+BOARD_MAIN_PARTITION_LIST := product vendor system
+BOARD_USES_METADATA_PARTITION := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 loop.max_part=70 buildvariant=eng
-BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=bootloader
-BOARD_KERNEL_IMAGE_NAME := Image
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 buildvariant=user  loop.max_part=70 
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x0bc08000
-BOARD_RAMDISK_OFFSET     := 0x07c08000
+BOARD_RAMDISK_OFFSET := 0x07c08000
 BOARD_DTB_OFFSET := 0x0bc08000
-BOARD_KERNEL_SECOND_OFFSET := 0x00f00000
+BOARD_KERNEL_SECOND_OFFSET := 0xbff88000
 BOARD_BOOTIMG_HEADER_VERSION := 2
 
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo
 
-## Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE     := 67108864
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
-BOARD_CACHEIMAGE_PARTITION_SIZE    := 268435456
-BOARD_PERSISTIMAGE_PARTITION_SIZE  := 33554432
-#BOARD_CUSTIMAGE_PARTITION_SIZE     := 536870912
-BOARD_VENDORIMAGE_PARTITION_SIZE   := 536870912
-BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 3221225472
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 58846064640 # (58846081024-16384)
-BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+# Image
+BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/dtb
+BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
+BOARD_FLASH_BLOCK_SIZE := 131072
 
-## Recovery
-#BOARD_HAS_NO_SELECT_BUTTON := true
-#BOARD_HAS_LARGE_FILESYSTEM := true
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+# Platform
+TARGET_BOARD_PLATFORM := mt6768
+TARGET_BOOTLOADER_BOARD_NAME := mt6768
 
-# System as root
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
+
+# Crypto
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
-TW_INCLUDE_CRYPTO := true
-TW_MAX_BRIGHTNESS := 255
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_SCREEN_BLANK_ON_BOOT := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-BOARD_SUPPRESS_SECURE_ERASE := true
-TW_IGNORE_MISC_WIPE_DATA := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_NO_EXFAT_FUSE := true
 TW_EXTRA_LANGUAGES := true
-
-# Supress error messages while building
-ALLOW_MISSING_DEPENDENCIES := true
-
-#enable Logcat in twrp
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_USE_TOOLBOX := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_Y_OFFSET := 80
+TW_H_OFFSET := -80
 TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
+TARGET_USES_LOGD := true 
+TARGET_USES_MKE2FS := true
+TW_EXCLUDE_TWRPAPP := true
+TW_SKIP_COMPATIBILITY_CHECK := true
 
-## Treble
-BOARD_NEEDS_VENDORIMAGE_SYMLINK := false
-TARGET_COPY_OUT_VENDOR := vendor
+# System as root
+BOARD_SUPPRESS_SECURE_ERASE := true
 
-TW_DEVICE_VERSION := Tostisto
+# Selinux
+BOARD_SEPOLICY_VERS := 29.0.3
+SEPOLICY_IGNORE_NEVERALLOWS := true
+SELINUX_IGNORE_NEVERALLOWS := true
 
-#SHRP_Variables
-SHRP_PATH := device/xiaomi/markw
-SHRP_MAINTAINER := Tostisto
-SHRP_DEVICE_CODE := markw
-SHRP_EXTERNAL := /external_sd
-SHRP_INTERNAL := /sdcard
-SHRP_OTG := /usbotg
-SHRP_REC := /dev/block/mmcblk0p22
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
+
+#SHRP Specific Stuff
+
+SHRP_PATH := device/xiaomi/merlin
+SHRP_MAINTAINER := Box-Boi
+SHRP_DEVICE_CODE := RMX2151
 SHRP_REC_TYPE := Treble
-SHRP_DEVICE_TYPE := A-Only
+SHRP_DEVICE_TYPE := A_only
+SHRP_NOTCH := true
+SHRP_EDL_MODE := 1
+SHRP_EXTERNAL := /UFS_SD
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usb_otg
 SHRP_FLASH := 1
-SHRP_FONP_1 := /sys/class/leds/led:torch_0/brightness
-SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
-SHRP_FONP_3 := /sys/class/leds/led:switch/brightness
-SHRP_FLASH_MAX_BRIGHTNESS := 200
+SHRP_REC := /dev/block/by-name/recovery
+INC_IN_REC_MAGISK := true
+SHRP_DARK := true
 SHRP_OFFICIAL := true
-SHRP_EXPRESS := true
+INC_IN_REC_ADDON_1 := true
+INC_IN_REC_ADDON_2 := true
+INC_IN_REC_ADDON_3 := true
+INC_IN_REC_ADDON_4 := true
+
+#SHRP Addons
+
+SHRP_EXTERNAL_ADDON_PATH := "device/xiaomi/merlin/addon/"
+SHRP_EXTERNAL_ADDON_1_NAME := "SELinux Permissiver"
+SHRP_EXTERNAL_ADDON_1_INFO := "Makes SELinux Permissive"
+SHRP_EXTERNAL_ADDON_1_FILENAME := "permissiver.zip"
+SHRP_EXTERNAL_ADDON_1_BTN_TEXT := "Flash"
+SHRP_EXTERNAL_ADDON_1_SUCCESSFUL_TEXT := "Flashed Successfully"
+SHRP_INC_IN_REC_EXTERNAL_ADDON_1 := true
+SHRP_EXTERNAL_ADDON_2_NAME := "SELinux Enforcer"
+SHRP_EXTERNAL_ADDON_2_INFO := "Reverts Back to Enforcing SELinux"
+SHRP_EXTERNAL_ADDON_2_FILENAME := "enforcer.zip"
+SHRP_EXTERNAL_ADDON_2_BTN_TEXT := "Flash"
+SHRP_EXTERNAL_ADDON_2_SUCCESSFUL_TEXT := "Flashed Successfully"
+SHRP_INC_IN_REC_EXTERNAL_ADDON_2 := true
+SHRP_EXTERNAL_ADDON_3_NAME := "Unlock Logo Remover"
+SHRP_EXTERNAL_ADDON_3_INFO := "Removes Unlock Logo on Redmi Splash Screen"
+SHRP_EXTERNAL_ADDON_3_FILENAME := "Unlock-Logo-Remover.zip"
+SHRP_EXTERNAL_ADDON_3_BTN_TEXT := "Flash"
+SHRP_EXTERNAL_ADDON_2_SUCCESSFUL_TEXT := "Flashed Successfully"
+SHRP_INC_IN_REC_EXTERNAL_ADDON_3 := true
+SHRP_EXTERNAL_ADDON_4_NAME := "Imei Backup"
+SHRP_EXTERNAL_ADDON_4_INFO := "Takes Imei backup"
+SHRP_EXTERNAL_ADDON_4_FILENAME := "Imei-Backup.zip"
+SHRP_EXTERNAL_ADDON_4_BTN_TEXT := "Flash"
+SHRP_EXTERNAL_ADDON_4_SUCCESSFUL_TEXT := "Flashed Successfully"
+SHRP_INC_IN_REC_EXTERNAL_ADDON_4 := true
+SHRP_EXTERNAL_ADDON_5_NAME := "[MIUI] Disable Replace SHRP"
+SHRP_EXTERNAL_ADDON_5_INFO := "Flash in MIUI To Stop MIUI Recovery From Replacing"
+SHRP_EXTERNAL_ADDON_5_FILENAME := "disable-replace-shrp.zip"
+SHRP_EXTERNAL_ADDON_5_BTN_TEXT := "Flash"
+SHRP_EXTERNAL_ADDON_5_SUCCESSFUL_TEXT := "Installed"
+SHRP_INC_IN_REC_EXTERNAL_ADDON_5 := true
+SHRP_EXTERNAL_ADDON_6_NAME := "[MAGISK] Safetynet Fix"
+SHRP_EXTERNAL_ADDON_6_INFO := "A magisk module which attempts to fix Safetynet"
+SHRP_EXTERNAL_ADDON_6_FILENAME := "safetynet-fix.zip"
+SHRP_EXTERNAL_ADDON_6_BTN_TEXT := "Flash"
+SHRP_EXTERNAL_ADDON_6_SUCCESSFUL_TEXT := "Flashed Successfully"
+SHRP_INC_IN_REC_EXTERNAL_ADDON_6 := true
